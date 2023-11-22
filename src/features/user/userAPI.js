09ad6f5c94d5338de952c6a50e717ec98407c4f7
@Faimasoft786 <FatimaSoft786@gmail.com>
2023-11-22 15:28:48 +0500

    Git initial

---
+++ src/features/user/userAPI.js
@@ -0,0 +1,32 @@
+export function fetchLoggedInUserOrders() {
+  return new Promise(async (resolve) =>{
+    const response = await fetch('https://e-commerce-backend-lake.vercel.app/orders/own/') 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
+}
+
+
+export function fetchLoggedInUser() {
+  return new Promise(async (resolve) =>{
+    const response = await fetch('https://e-commerce-backend-lake.vercel.app/users/own') 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
+}
+
+export function updateUser(update) {
+  return new Promise(async (resolve) => {
+    const response = await fetch('https://e-commerce-backend-lake.vercel.app/users/'+update.id, {
+      method: 'PATCH',
+      body: JSON.stringify(update),
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+
